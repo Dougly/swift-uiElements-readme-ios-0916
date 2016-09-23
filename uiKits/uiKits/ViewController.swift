e49ff5a232b8ddbd458ffac2c8fbdacdc5074ebe
Dougly <dgalante11@gmail.com>
2016-09-23 14:19:19 -0400

    messing around with UIKit

---
+++ uiKits/uiKits/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  uiKits
+//
+//  Created by Douglas Galante on 9/23/16.
+//  Copyright © 2016 Douglas Galante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
